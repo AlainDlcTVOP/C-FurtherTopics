837c3bdd2dbbf37ce7bb8bc6d55b20662ed18fc0
AlainDlcTVOP <alain_dlc@outlook.com>
2020-02-16 16:46:25 +0100

    ternary ex1

---
+++ AssignmentOperator/ternary1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdint.h>
+
+
+/* Basic ternary*/
+
+int main()
+{
+    int a, b, larger;
+
+    printf("Enter a value :\n");
+    scanf("%d", &a);
+    printf("Enter another value :\n");
+    scanf("%d", &b);
+
+    larger = (a > b) ? a : b;
+    
+
+    printf("%d is the larger value\n",larger);
+
+    return 0;
+}
